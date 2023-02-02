e07db1209ecfe45f35ee7e83d71acf767efcf42a
dongheonchoi <124031425+dongheonchoi@users.noreply.github.com>
2023-02-02 17:38:53 +0900

    4일차
    4일차 js 사용법을 공부했습니다

---
+++ day04/out.js
@@ -0,0 +1,32 @@
+function 빨래하다( ) {
+
+    console.log('1. 먼지를 털자');
+    console.log('2. 물에 집어 넣는다.');
+    console.log('3. 비누칠을 한다.');
+    console.log('4. 빨래질을 한다.');
+    console.log('5. 물에 헹군다.');
+    console.log('6. 짠다');
+    console.log('7. 넌다');
+
+}
+
+
+
+function off() {
+
+    alert('off')
+
+}
+
+off()
+
+
+function cleaning() {
+
+    console.log('1. 물건들을 치운다.')
+    console.log('2. 청소기를 돌린다.')
+    console.log('3. 걸레질을 한다.')
+    console.log('4. 뒷정리를 한다.')
+
+}
+
---
+++ day04/정리문제/out2.js
@@ -0,0 +1,16 @@
+function 여행하다() {
+
+    console.log("1. 여행 계획을 짠다")
+    console.log("2. 짐들을 챙긴다.")
+    console.log("3. 여행에 떠난다.")
+    
+}
+
+
+function 쉬다() { 
+    
+    document.write('1. 편한 옷으로 갈아 입는다.')
+    document.write('2. 조용한 환경을 만든다.')
+    document.write('3. 좋아 하는 영상을 틀고 편히 쉰다.')
+    
+}
